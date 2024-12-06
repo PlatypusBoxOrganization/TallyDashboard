fe545c0ae994e40d0e292f8393e58a385deeb790
Platypus <yuvrajphoenix@gmail.com>
2024-12-06 17:49:06 +0530

    Updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
+    define: {
+        'process.env': process.env,
     },
-  },
-  optimizeDeps: {
-    include: [
-      '@tailwindConfig',
-    ]
-  }, 
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    }
-  } 
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
+        },
+    },
+    optimizeDeps: {
+        include: ['@tailwindConfig'],
+    },
+    build: {
+        outDir: 'dist', // Output directory for build files
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+    base: '/TallyDashboard/', // Replace with your repository name
+});
