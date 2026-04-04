77f6ce949f3b48fa0af181c0714a1294360ab8e1
Platypus <platypus@platypusbox.com>
2026-04-04 16:21:42 +0530

    settings option removed

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -172,35 +172,35 @@ function Sidebar({
               </li>
 
               {/* Settings */}
-              <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname.includes('settings') && 'bg-slate-200 dark:bg-gray-900'}`}>
-                <NavLink
-                  end
-                  to="/settings"
-                  className={`block text-slate-900 dark:text-slate-200 hover:text-slate-900 dark:hover:text-white truncate transition duration-150`}
-                >
-                  <div className="flex items-center">
-                    <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                      <path
-                        className={`fill-current ${pathname.includes('settings') ? 'text-indigo-500' : 'text-slate-600 dark:text-gray-600'}`}
-                        d="M19.714 14.7l-7.007 7.007-1.414-1.414 7.007-7.007c-.195-.4-.298-.84-.3-1.286a3 3 0 113 3 2.969 2.969 0 01-1.286-.3z"
-                      />
-                      <path
-                        className={`fill-current ${pathname.includes('settings') ? 'text-indigo-300' : 'text-slate-400 dark:text-gray-400'}`}
-                        d="M10.714 18.3c.4-.195.84-.298 1.286-.3a3 3 0 11-3 3c.002-.446.105-.894.3-1.286l-6.007-6.007 1.414-1.414 6.007 6.007z"
-                      />
-                      <path
-                        className={`fill-current ${pathname.includes('settings') ? 'text-indigo-600' : 'text-slate-600 dark:text-gray-500'}`}
-                        d="M5.7 10.714c.195.4.298.84.3 1.286a3 3 0 11-3-3c.446.002.894.105 1.286.3l7.007-7.007 1.414 1.414L5.7 10.714z"
-                      />
-                      <path
-                        className={`fill-current ${pathname.includes('settings') ? 'text-indigo-500' : 'text-slate-500 dark:text-gray-400'}`}
-                        d="M19.707 9.292a3.012 3.012 0 00-1.415 1.415L13.286 5.7c-.4.195-.84.298-1.286.3a3 3 0 113-3 2.969 2.969 0 01-.3 1.286l5.007 5.006z"
-                      />
-                    </svg>
-                    <span className="text-sm font-medium ml-3 lg:opacity-100 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Settings</span>
-                  </div>
-                </NavLink>
-              </li>
+              {/*<li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname.includes('settings') && 'bg-slate-200 dark:bg-gray-900'}`}>*/}
+              {/*  <NavLink*/}
+              {/*    end*/}
+              {/*    to="/settings"*/}
+              {/*    className={`block text-slate-900 dark:text-slate-200 hover:text-slate-900 dark:hover:text-white truncate transition duration-150`}*/}
+              {/*  >*/}
+              {/*    <div className="flex items-center">*/}
+              {/*      <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">*/}
+              {/*        <path*/}
+              {/*          className={`fill-current ${pathname.includes('settings') ? 'text-indigo-500' : 'text-slate-600 dark:text-gray-600'}`}*/}
+              {/*          d="M19.714 14.7l-7.007 7.007-1.414-1.414 7.007-7.007c-.195-.4-.298-.84-.3-1.286a3 3 0 113 3 2.969 2.969 0 01-1.286-.3z"*/}
+              {/*        />*/}
+              {/*        <path*/}
+              {/*          className={`fill-current ${pathname.includes('settings') ? 'text-indigo-300' : 'text-slate-400 dark:text-gray-400'}`}*/}
+              {/*          d="M10.714 18.3c.4-.195.84-.298 1.286-.3a3 3 0 11-3 3c.002-.446.105-.894.3-1.286l-6.007-6.007 1.414-1.414 6.007 6.007z"*/}
+              {/*        />*/}
+              {/*        <path*/}
+              {/*          className={`fill-current ${pathname.includes('settings') ? 'text-indigo-600' : 'text-slate-600 dark:text-gray-500'}`}*/}
+              {/*          d="M5.7 10.714c.195.4.298.84.3 1.286a3 3 0 11-3-3c.446.002.894.105 1.286.3l7.007-7.007 1.414 1.414L5.7 10.714z"*/}
+              {/*        />*/}
+              {/*        <path*/}
+              {/*          className={`fill-current ${pathname.includes('settings') ? 'text-indigo-500' : 'text-slate-500 dark:text-gray-400'}`}*/}
+              {/*          d="M19.707 9.292a3.012 3.012 0 00-1.415 1.415L13.286 5.7c-.4.195-.84.298-1.286.3a3 3 0 113-3 2.969 2.969 0 01-.3 1.286l5.007 5.006z"*/}
+              {/*        />*/}
+              {/*      </svg>*/}
+              {/*      <span className="text-sm font-medium ml-3 lg:opacity-100 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Settings</span>*/}
+              {/*    </div>*/}
+              {/*  </NavLink>*/}
+              {/*</li>*/}
             </ul>
           </div>
         </div>
